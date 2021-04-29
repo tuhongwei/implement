cdc8323934d33f1ff3110e9863a396e06c3a9ea6
Twittytop <1690542111@qq.com>
2021-04-30 01:12:06 +0800

    fix(promise): 修改promise的问题

--- promise/promise.js
+++ promise/promise.js
@@ -1,134 +1,124 @@
-/**
- * promise实现
- */
-
-const isFunction = (val) => {
-  return typeof val === 'function';
-};
-
-function Promise (Fn) {
-  // 初始为'PENDING'状态
-  this.status = 'PENDING';
-  this.value;
-  this.reason;
-  // 存储onFulfilled
-  this.resolves = [];
-  // 存储onRejected
-  this.rejects = [];
-  // let resolve = (value) => {
-  //   if (this.status === 'PENDING') {
-  //     // 加上异步处理，保证then方法先执行
-  //     setTimeout(() => {
-  //       //状态转换为FULFILLED
-  //       //执行then时保存到resolves里的回调
-  //       //如果回调有返回值，更新当前value
-  //       this.status = 'FULFILLED';
-  //       this.resolves.forEach(fn => {
-  //         value = fn(value) || value;
-  //       });
-  //       this.value = value;
-  //     });
-  //   }
-  // }
-  // let reject = (reason) => {
-  //   if (this.status === 'PENDING') {
-  //     // 加上异步处理，保证reject方法先执行
-  //     setTimeout(() => {
-  //       //状态转换为REJECTED
-  //       //执行then时保存到rejects里的回调
-  //       //如果回调有返回值，更新当前reason
-  //       this.status = 'REJECTED';
-  //       this.rejects.forEach(fn => {
-  //         reason = fn(reason) || reason;
-  //       });
-  //       this.reason = reason;
-  //     });
-  //   }
-  // }
-  let transition = (status, val) => {
-    if (this.status === 'PENDING') {
-      // 加上异步处理，保证方法先执行
-      setTimeout(() => {
-        //状态转换为REJECTED
-        //执行then时保存到rejects里的回调
-        //如果回调有返回值，更新当前reason
-        this.status = status;
-        let f = status === 'FULFILLED',
-        queue = this[f ? 'resolves' : 'rejects'];
-        queue.forEach(fn => {
-          val = fn(val) || val;
-        });
-        this[f ? 'value' : 'reason'] = val;
-      });
-    }
-  }
-  let resolve = (val) => {
-    transition('FULFILLED', val);
-  }
-  let reject = (reason) => {
-    transition('REJECTED', reason);
-  }
-  try {
-    Fn(resolve, reject);
-  } catch (err) {
-    reject(err);
-  }
+function isFunction (obj) {
+    return typeof obj === 'function';
 }
-
-Promise.prototype.then = (onFulfilled, onRejected) => {
-  let promise = this;
-  return new Promise((resolve, reject) => {
-    function success (value) {
-      let val = typeof onFulfilled === 'function' && onFulfilled(value) || value;
-      if (val && typeof val['then'] === 'function') {
-        val.then(value => {
-          resolve(value);
-        }, reason => {
-          reject(reason);
-        });
-      } else {
-        resolve(val);
+function isObject (obj) {
+    return !!(obj && typeof obj === 'object');
+}
+function isPromise (obj) {
+    return obj instanceof Promise;
+}
+function isThenable (obj) {
+    return (isFunction(obj) || isObject(obj)) && 'then' in obj;
+}
+function transition (promise, state, result) {
+    if (promise.state !== 'pending') return;
+    promise.state = state;
+    promise.result = result;
+    setTimeout(() => promise.callbacks.forEach(callback => handleCallback(callback, state, result)));
+}
+function resolvePromise (promise, result, resolve, reject) {
+    if (promise === result) {
+        return reject(new TypeError('Chaining cycle detected for promise'));
+    } 
+    if (isPromise(result)) {
+        return result.then(resolve, reject);
+    } 
+    if (isThenable(result)) {
+      try {
+        let then = result.then;
+        if (isFunction(then)) {
+          return new Promise(then.bind(result)).then(resolve, reject);
+        }
+      } catch (error) {
+        return reject(error);
       }
     }
-    function erro (reason) {
-      let rea = typeof onRejected === 'function' && onRejected(reason) || reason;
-      reject(reason);
+    resolve(result);
+}
+function handleCallback (callback, state, result) {
+    let { onFulfilled, onRejected, resolve, reject } = callback;
+    try {
+        if (state === 'fulfilled') {
+            isFunction(onFulfilled) ? resolve(onFulfilled(result)) : resolve(result);
+        } else if (state === 'rejected') {
+            isFunction(onRejected) ? resolve(onRejected(result)) : reject(result);
+        }
+    } catch (e) {
+        reject(e);
     }
-    if (promise.status === 'PENDING') {
-      promise.resolves.push(success);
-      promise.rejects.push(erro);
-    } else if (promise.status === 'FULFILLED'){
-      console.log('FULFILLED')
-      success(promise.value);
-    } else if (promise.status === 'REJECTED'){
-      onsole.log('REJECTED')
-      erro(promise.reason);
+}
+class Promise {
+    constructor (executor) {
+        this.state = 'pending';
+        this.result = undefined;
+        this.callbacks = [];
+        let onFulfilled = value => transition(this, 'fulfilled', value);
+        let onRejected = reason => transition(this, 'rejected', reason);
+        // 保证 resolve 或 reject 只有一次调用
+        let flag = false;
+        let resolve = value => {
+            if (flag) return;
+            flag = true;
+            resolvePromise(this, value, onFulfilled, onRejected);
+        };
+        let reject = reason => {
+            if (flag) return;
+            flag = true;
+            onRejected(reason);
+        };
+        try {
+           executor(resolve, reject); 
+        } catch (e) {
+            reject(e);
+        }
     }
+    then (onFulfilled, onRejected) {
+        return new Promise((resolve, reject) => {
+            let callback = { onFulfilled, onRejected, resolve, reject };
+            if (this.state === 'pending') {
+                this.callbacks.push(callback);
+            } else {
+                setTimeout(() => {
+                    handleCallback(callback, this.state, this.result);
+                });
+            }
+        });
+    }
+    catch (onRejected) {
+        this.then(undefined, onRejected);
+    }
+    finally (onFinally) {
+        
+    }
+    static resolve (value) {
+        if (isPromise(value)) return value;
+        return new Promise ((resolve, reject) => resolve(val));
+    }
+    static reject (reason) {
+        return new Promise ((resolve, reject) => reject(reason));
+    }
+    static all (iterable) {
+        
+    }
+    static allSettled (iterable) {
+        
+    }
+    static any (iterable) {
+        
+    }
+    static race (iterable) {
+        
+    }
+}
+// 目前是通过他测试 他会测试一个对象
+// 语法糖
+Promise.defer = Promise.deferred = function () {
+  let dfd = {}
+  dfd.promise = new Promise((resolve,reject)=>{
+    dfd.resolve = resolve;
+    dfd.reject = reject;
   });
+  return dfd;
 }
-Promise.prototype.catch = (onRejected) => {
-  return this.then(null, onRejected);
-}
-function getInfo(success, fail) {
-  return new Promise((resolve, reject) => {
-    setTimeout(_ => {
-      let ran = Math.random();
-      console.log(success, ran);
-      if (ran > 0.5) {
-        resolve(success);
-      } else {
-        reject(fail);
-      }
-    }, 200);
-  })
-}
-getInfo('Vchat', 'fail').then(res => {
-  console.log(res);
-  return getInfo('可以线上预览了', 'erro');
-}, rej => {
-  console.log(rej);
-}).then(res => {
-  console.log(res);
-}, rej => {
-  console.log(rej);
-});
+module.exports = Promise;
+//npm install promises-aplus-tests 用来测试自己的promise 符不符合promisesA+规范
